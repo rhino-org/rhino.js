7e98dc463463629920b0dc698b6fcb80ee3cb80f
Bruno Gonçalves <bugotech@gmail.com>
2021-02-01 15:13:24 -0300

    add num.percentual e porcentagem, round

--- support/num.js
+++ support/num.js
@@ -1,5 +1,4 @@
-class Num
-{
+class Num {
     /**
      * Gerar numero randomico.
      *
@@ -23,6 +22,48 @@ class Num
         max = Math.floor(max);
 
         return Math.floor(this.random(min, max));
+    }
+
+    /**
+     * Arredondar um valor pelas casas de precisão.
+     * 
+     * @param {Number} value Valor a ser arredondado
+     * @param {Number} precisao Numero de casas de precisao
+     * @returns {Number}
+     */
+    static round(value, precisao = 2) {
+        var fator = Math.pow(10, precisao);
+        var temp = value * fator;
+        return Math.round(temp) / fator;
+    }
+
+    /**
+     * Retorna a porcentagem de uma fraçao sobre o valor total.
+     *
+     * @param npart
+     * @param value
+     * @param int dec
+     * @param int round
+     * @return float|int
+     */
+    static percentage(npart, value, dec = 2) {
+        if (value <= 0) {
+            return 0;
+        }
+
+        return this.round((npart * 100) / value, dec);
+    }
+
+    /**
+     * Retorna o valor obtido pelo percentual sobre um valor.
+     *
+     * @param percentage
+     * @param value
+     * @param int dec
+     * @return Number
+     */
+    static percent(percentage, value, dec = 2) {
+        return this.round((value * percentage) / 100, dec);
     }
 }
 
