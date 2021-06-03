42c08a242c764e5abf57b0b301a5b9776472eb17
Bruno Gonçalves <bugotech@gmail.com>
2021-06-03 00:14:24 -0300

    #build-js 1.8.2 add funcs carbon

--- support/carbon.js
+++ support/carbon.js
@@ -1,4 +1,5 @@
-const df = require('date-format');
+//const df = require('date-format');
+const _dt = require('date-and-time');
 
 class Carbon
 {
@@ -7,7 +8,8 @@ class Carbon
      * @returns {string}
      */
     static get FORMAT_DEFAULT() {
-        return 'yyyy-MM-dd hh:mm:ss';
+        return 'YYYY-MM-DD HH:mm:ss';
+        //return 'yyyy-MM-dd hh:mm:ss';
     }
 
     /**
@@ -19,22 +21,38 @@ class Carbon
     }
 
     /**
-     * Converter um string em data e hora por um formato específico.
-     * @param {String} value Data e/ou hora em string a ser convertida
-     * @param {String} format Formato da data e hora 
+     * Retorna a data e hora de hoje.
      * @returns {Date}
      */
-    static createFromFormat(value, format) {
+    static today() {
+        var hoje = Carbon.format(Carbon.now(), 'YYYY-MM-DD');
+
+        return Carbon.createFromFormat(hoje + ' 00:00:00', 'YYYY-MM-DD HH:mm:ss');
+    }
+
+    /**
+     * Converter um string em data e hora por um formato específico.
+     * 
+     * @param {String} value Data e/ou hora em string a ser convertida
+     * @param {String} format Formato da data e hora 
+     * @param {Boolean} utc
+     * @returns {Date}
+     */
+    static createFromFormat(value, format, utc = true) {
         format = ((format == undefined) || (format == '')) ? this.FORMAT_DEFAULT : format;
-        return df.parse(format, value);
+
+        //return df.parse(format, value);
+        return _dt.parse(value, format, utc);
     }
 
     /**
      * Converter um data e hora em string em um formato especifico.
      * @param {Date} dt Data e hora
      * @param {String} format Formato a converter em string
+     * @param {Boolean} utc
+     * @returns {String}
      */
-    static format(dt, format) {
+    static format(dt, format, utc = true) {
         // Veriifcar se o dt foi informado como numerico
         dt = (typeof dt == 'number') ? new Date(dt) : dt;
 
@@ -42,7 +60,114 @@ class Carbon
         format = ((format == undefined) || (format == '')) ? this.FORMAT_DEFAULT : format;
 
         // Converter
-        return df.asString(format, dt);
+        //return df.asString(format, dt);
+        return _dt.format(dt, format, utc);
+    }
+
+    /**
+     * Adicionar anos.
+     * 
+     * @param {Date} dt Data e Hora
+     * @param {Number} value Qtdade a ser somada
+     * @returns {Date}
+     */
+    static addYears(dt, value) {
+        return Carbon.__addFunc('addYears', dt, value);
+    }
+
+    /**
+     * Adicionar meses.
+     * 
+     * @param {Date} dt Data e Hora
+     * @param {Number} value Qtdade a ser somada
+     * @returns {Date}
+     */
+    static addMonths(dt, value) {
+        return Carbon.__addFunc('addMonths', dt, value);
+    }
+
+    /**
+     * Adicionar dias.
+     * 
+     * @param {Date} dt Data e Hora
+     * @param {Number} value Qtdade a ser somada
+     * @returns {Date}
+     */
+    static addDays(dt, value) {
+        return Carbon.__addFunc('addDays', dt, value);
+    }
+
+    /**
+     * Adicionar horas.
+     * 
+     * @param {Date} dt Data e Hora
+     * @param {Number} value Qtdade a ser somada
+     * @returns {Date}
+     */
+    static addHours(dt, value) {
+        return Carbon.__addFunc('addHours', dt, value);
+    }
+
+    /**
+     * Adicionar minutos.
+     * 
+     * @param {Date} dt Data e Hora
+     * @param {Number} value Qtdade a ser somada
+     * @returns {Date}
+     */
+    static addMinutes(dt, value) {
+        return Carbon.__addFunc('addMinutes', dt, value);
+    }
+
+    /**
+     * Adicionar segundos.
+     * 
+     * @param {Date} dt Data e Hora
+     * @param {Number} value Qtdade a ser somada
+     * @returns {Date}
+     */
+    static addSeconds(dt, value) {
+        return Carbon.__addFunc('addSeconds', dt, value);
+    }
+
+    /**
+     * Adicionar milisegundos.
+     * 
+     * @param {Date} dt Data e Hora
+     * @param {Number} value Qtdade a ser somada
+     * @returns {Date}
+     */
+    static addMilliseconds(dt, value) {
+        return Carbon.__addFunc('addMilliseconds', dt, value);
+    }
+
+    /**
+     * Subtrair uma data da outra.
+     * 
+     * @param {Date} dt1 Data 1
+     * @param {Date} dt2 Data 2
+     * @returns {Date}
+     */
+    static subtract(dt1, dt2) {
+        // Veriifcar se o dt foi informado como numerico
+        dt1 = (typeof dt1 == 'number') ? new Date(dt1) : dt1;
+        dt2 = (typeof dt2 == 'number') ? new Date(dt2) : dt2;
+
+        return _dt.subtract(dt1, dt2);
+    }
+
+    /**
+     * Executar função de add value a data.
+     * 
+     * @param {Date} dt Data e Hora
+     * @param {Number} value Qtdade a ser somada
+     * @returns {Date}
+     */
+     static __addFunc(func, dt, value) {
+        // Veriifcar se o dt foi informado como numerico
+        dt = (typeof dt == 'number') ? new Date(dt) : dt;
+
+        return _dt[func](dt, value);
     }
 }
 
