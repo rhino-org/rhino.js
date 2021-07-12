2241c3f97257d9f03f22372e52a28076d5902bff
Bruno Gonçalves <bugotech@gmail.com>
2021-07-12 14:59:16 -0300

    add index

--- support/index.js
+++ support/index.js
@@ -2,9 +2,10 @@
  * Exports root.
  */
 module.exports = {
-    Arr: require('./arr'),
-    Carbon: require('./carbon'),
-    Num: require('./num'),
-    Str: require('./str'),
-    Obj: require('./obj')
+    Arr    : require('./arr'),
+    Carbon : require('./carbon'),
+    Num    : require('./num'),
+    Str    : require('./str'),
+    Obj    : require('./obj'),
+    Thread : require('./thread')
 };
